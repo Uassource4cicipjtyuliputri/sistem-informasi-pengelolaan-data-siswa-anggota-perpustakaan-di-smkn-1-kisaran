61ab4c7d74d7ef9208e5d869214db6a8b249905f
Uassource4cicipjtyuliputri <60250114+Uassource4cicipjtyuliputri@users.noreply.github.com>
2020-01-24 15:17:30 +0700

    Add files via upload

---
+++ report/arsip_semua.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cetak Data Anggota</title>
+        <link href="../Assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>  
+    <body onload="print()">
+        <!--Menampilkan data detail arsip-->
+        <?php
+        include '../config/koneksi.php';
+        ?>   
+
+        <div class="container">
+            <div class='row'>
+                <div class="col-sm-12">
+                    <!--dalam tabel--->
+                    <div class="text-center">
+                        <h2>Sistem Informasi Pendataan Sistem Informasi Pengolahan Data Siswa Anggota Perpustakaan SMKN 1 Kisaran </h2>
+                        <h4>Jl.Akasia, Mekar Baru, Kec.Kota Kisaran Barat, Kabupaten Asahan, Sumatera Utara  Kode Pos : 21211</h4>
+                        <hr>
+                        <h3>DATA SELURUH ANGGOTA</h3>
+                        <table class="table table-bordered table-striped table-hover"> 
+                        <tbody>
+                                <thead>
+								<tr>
+									<th>No.</th><th>Nomor Anggota</th><th>Nama Anggota</th><th>Tanggal Lahir</th><th>Kelas</th>
+								</tr>
+								</thead>
+							<tbody>
+                            <!--ambil data dari database, dan tampilkan kedalam tabel-->
+                            <?php
+                            //buat sql untuk tampilan data, gunakan kata kunci select
+                            $sql = "SELECT * FROM tbl_anggota";
+                            $query = mysqli_query($koneksi, $sql) or die("SQL Anda Salah");
+                            //Baca hasil query dari databse, gunakan perulangan untuk 
+                            //Menampilkan data lebh dari satu. disini akan digunakan
+                            //while dan fungdi mysqli_fecth_array
+                            //Membuat variabel untuk menampilkan nomor urut
+                            $nomor = 0;
+                            //Melakukan perulangan u/menampilkan data
+                            while ($data = mysqli_fetch_array($query)) {
+                                $nomor++; //Penambahan satu untuk nilai var nomor
+                                ?>
+                                <tr>
+                                    <td><?= $nomor ?></td>
+									<<td><?= $data['nomor_anggota'] ?></td>
+									<<td><?= $data['nama_anggota'] ?></td>
+									<td><?= $data['ttl'] ?></td>
+									<td><?= $data['kelas'] ?></td>
+                                </tr>
+                                <!--Tutup Perulangan data-->
+                            <?php } ?>
+							</tbody>
+                        </tbody>
+                            <tfoot> 
+                                <tr>
+                                    <td colspan="2" class="text-right">
+                                        Kisaran  <?= date("d-m-Y") ?>
+                                        <br><br><br><br>
+                                        <u>Drs.Edu Butar Butar<strong></u><br>
+									</td>
+								</tr>
+							</tfoot> 
+                        </table>
+                    </div>
+                </div>
+            </div>
+    </body>
+</html>
