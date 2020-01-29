fad0819797b9c856d67dfe1714a906fbcec0df0e
Uassource4cicipjtyuliputri <60250114+Uassource4cicipjtyuliputri@users.noreply.github.com>
2020-01-29 10:09:15 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            Copyright &COPY; <?= date('Y')?> SMKN 1 Kisaran |
+            Copyright &COPY; <?= date('Y')?> CICI PARAMITA PANJAITAN , YULIA NINGSIH , PUTRI SEPTIA ANANDA|
             <a href="#">Dokumentasi</a>
             <a href="#">Peta Situs</a>
             <a href="#">Support</a>
